088b36e74960fe1adbdbefcb0868b3b9c03c8e9f
Nathan <146884027+natmfat@users.noreply.github.com>
2024-10-12 11:44:36 -0700

    feat: adding calc

---
+++ AddNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cout << "Enter two numbers" << endl;
+  cin >> a >> b;
+  c = a + b;
+  cout << "The result of " << a << " + " << b << " is " << c << "." << endl;
+  return 0;
+}
